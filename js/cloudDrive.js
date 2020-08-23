2b0294a36bb2f5b267d268d9cb519f81b3d90744
William <100401031@mail2.nccu.tw>
2020-08-24 04:23:20 +0800

    Update 2020-08-23T20:23:18.560Z

--- js/cloudDrive.js
+++ js/cloudDrive.js
@@ -1,2 +1,2 @@
-"use strict";var vm;$(function(){$.contextMenu({selector:".upload_button",callback:function(n){var e="clicked: "+n;window.console,alert(e)},items:{share:{name:"共享"},download:{name:"下載"},star:{name:"標示星號"},rename:{name:"重新命名"},move:{name:"移動"},copy:{name:"複製"},delete:{name:"刪除"}}}),$(".upload_button").on("click",function(n){})}),$(function(){vm=new Vue({el:"#app",data:{},mounted:function(){}})});
+"use strict";var vm;$(function(){vm=new Vue({el:"#app",data:{},mounted:function(){}})});
 //# sourceMappingURL=cloudDrive.js.map
